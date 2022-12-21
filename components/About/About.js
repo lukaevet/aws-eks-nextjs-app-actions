9485c00ae4a7994ce0e1a9d22e5f5a0eff5c97f6
Luka Evetovic <lukaevet@gmail.com>
2022-12-21 13:26:11 +0100

    aws eks cluster application v1.1

--- components/About/About.js
+++ components/About/About.js
@@ -17,21 +17,9 @@ export default function About() {
           <Text tag="h2">Ola 👋</Text>
           <Text>
             I'm Luka, a coffee lover ☕, 30-something-year-old
-            <b>front-end developer</b> 💻 with a background in computer and
-            electrical engineering. I am based out of sunny ☀️ Edinburgh,
-            Scotland and currently doing freelance work and consulting for
-            international clients.
+            <b>DevOps Engineer</b> 💻 
           </Text>
-          <Text>
-            I started creating cool web designs using HTML and CSS back in 2006.
-            I am JavaScript enthusiast and extremely passionate about coding. I
-            love to learn new things every day and keep up with the new
-            technologies. In many ways, websites and apps are my first love. 🧡
-          </Text>
-          <Text>
-            Last but not least, one of my favorite actuvutes is to educate and
-            mentor others in the field to fulfill their potential and deepen my
-            knowledge. You can find out more on my YouTube Channel.
+          <Text>🧡
           </Text>
         </div>
       </Container>
--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -6,30 +6,6 @@ import Divider from "../Divider/Divider";
 import SectionHeading from "../SectionHeading/SectionHeading";
 import ContactList from "../ContactList/ContactList";
 
-export default function Contact({ settings }) {
-  const items = [
-    {
-      icon: "email",
-      uri: `mailto:${settings.email}`,
-      text: settings.email
-    },
-    {
-      icon: "youtube",
-      uri: settings.youtube.uri,
-      text: `@${settings.youtube.username}`
-    },
-    {
-      icon: "dev",
-      uri: settings.dev.uri,
-      text: `@${settings.dev.username}`
-    },
-    {
-      icon: "twitter",
-      uri: settings.twitter.uri,
-      text: `@${settings.twitter.username}`
-    }
-  ];
-
   return (
     <Section id="contact" className={styles.contact}>
       <Container className={styles.container}>
@@ -42,4 +18,4 @@ export default function Contact({ settings }) {
       <Divider />
     </Section>
   );
-}
+
